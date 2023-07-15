6e9a62a85d97c5a3059ca1a648f9e09c5913dc17
Ayush yadav <113254994+Ayushyadav04@users.noreply.github.com>
2023-07-16 02:02:54 +0530

    Update flask_app_.py

--- flask_app_.py
+++ flask_app_.py
@@ -1,71 +1,67 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
-from bson import ObjectId
 from pymongo import MongoClient
-
+from bson import ObjectId
+from flask_restful import Api, Resource
 
 app=Flask(__name__)                                           # Instance of the Flask application
+app.config['MONGO_URI']='mongodb://localhost:27017/app_data'  # URI
 client=MongoClient('mongodb://localhost:27017/app_data')      # mongoDB URI
 database=client['app_data']                                   # Database name
 collection=database['users']                                  # Collection name present in the database
+mongo=PyMongo(app)                                            # Instance of Pymongo class  
+api=Api(app)
 
 
-# Returns a list of all users.
-@app.route('/collection', methods=['GET'])              # "users" collection from database app_data
-def get_users():
-    users=collection.find()  # method find to collect all the data
-    user_list=[]
-    for i in users:
-        i['_id']=str(i['_id'])       # Convert objectID to string rep.
-        user_list.append(i)
-    return jsonify(user_list)
+        
+class UserResource(Resource):
+    # Returns a list of all users and specific user
+    def get(self,user_id=None):
+        if user_id:
+            object_id=ObjectId(user_id)      # convert user_id into objectID
+            #function to search provided ID if any otherwise display 404 not found error
+            user=collection.find_one({'_id': object_id})   
+            user['_id']=str(user['_id'])
+            return user
+        else:
+            users=collection.find()
+            user_list=[]
+            for user in users:
+                user['_id']=str(user['_id'])
+                user_list.append(user)
+            return user_list
+    
+    
+    # Creates a new user with the specified data.
+    def post(self):
+        new_user=request.get_json()
+        result=collection.insert_one(new_user)   # insert_one uses insert new user and get new unique ID
+        user_id=collection.find_one({'_id': result.inserted_id})
+        return str(user_id), 201   # 201 represent successful insertion
 
-
-# Returns the user with the specified ID.
-@app.route('/collection/<user_id>', methods=['GET'])
-def get_user(user_id):
-    object_id=ObjectId(user_id)      # convert user_id into objectID
-    #function to search provided ID if any otherwise display 404 not found error
-    user=collection.find_one({'_id': object_id})   
-    if user:
-        user['_id']=str(user['_id'])
-        return jsonify(user)
-    else:
-        return jsonify({'message': 'User not found'}), 404
-
-
-# Creates a new user with the specified data.
-@app.route('/collection', methods=['POST'])
-def create_user():
-    new_user={
-        'name': request.json['name'],'email': request.json['email'],'password': request.json['password']
-    }   # dictioanry represent new user data
-    user_id=collection.insert_one(new_user).inserted_id   # insert_one uses insert new user and get new unique ID
-    return jsonify(str(user_id)), 201   # 201 represent successful insertion
-
-
-# Updates the user with the specified ID with the new data
-@app.route('/collection/<string:user_id>', methods=['PUT'])
-def update_user(user_id):
-    object_id=ObjectId(user_id)
-    updated_user={
-        'name': request.json['name'],'email': request.json['email'],'password': request.json['password']
-    }    # dictionary to input updated data
-    # Now match the ID provided and using "$ser" update data with method update_one
-    result=collection.update_one({'_id': object_id}, {'$set': updated_user})
-    if result.modified_count==0:
-        return jsonify({'message': 'User not found'}), 404
-    return jsonify({'message': 'User updated'})
-
-
-# Deletes the user with the specified ID
-@app.route('/collection/<string:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    object_id=ObjectId(user_id)
-    result=collection.delete_one({'_id': object_id})
-    if result.deleted_count==0:
-        return jsonify({'message': 'User not found'}), 404
-    return jsonify({'message': 'User deleted'})
-
+    
+    # Updates the user with the specified ID with the new data
+    def put(self, user_id):
+        object_id=ObjectId(user_id)
+        updated_user={
+            'name': request.json['name'],'email': request.json['email'],'password': request.json['password']
+        }    # dictionary to input updated data
+        # Now match the ID provided and using "$ser" update data with method update_one
+        result=collection.update_one({'_id': object_id}, {'$set': updated_user})
+        if result.modified_count == 0:
+            return {'message': 'Same user found or User not found'}, 404
+        return {'message': 'User updated'}
+    
+    # Deletes the user with the specified ID
+    def delete(self,user_id):
+        object_id=ObjectId(user_id)
+        result=collection.delete_one({'_id': object_id})
+        if result.deleted_count==0:
+            return {'message': 'User not found'}, 404
+        return {'message': 'User deleted'}
+    
+api.add_resource(UserResource, '/collection', '/collection/<string:user_id>')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(5000),debug=True)
+    
